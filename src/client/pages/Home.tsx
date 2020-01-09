f1612585d2b364f58e6f505c272ec3230f5e0bad
buhtla85 <tamara.naunovic@gmail.com>
2020-01-09 21:54:04 +0100

    finished setup for home page

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import {Navbar} from "../components/Navbar";
+import {Hero} from "../components/Hero";
+import {SectionMission} from "../components/SectionMission";
+import {SectionServices} from "../components/SectionServices";
+import {SectionTestimonials} from "../components/SectionTestimonials";
+import {Footer} from "../components/Footer";
 
 export const Home = (): JSX.Element => (
     <React.Fragment>
         <Navbar />
+        <Hero />
+        <SectionMission />
+        <SectionServices />
+        <SectionTestimonials />
+        <Footer content="Copyright &copy; 2020, Tamara"/>
     </React.Fragment>
 );
