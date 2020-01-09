2e9ee493074db0487331e1c67047d70691bafcea
buhtla85 <tamara.naunovic@gmail.com>
2020-01-09 19:45:00 +0100

    added home page

---
+++ src/client/pages/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {Navbar} from "../components/Navbar";
+
+export const Home = (): JSX.Element => (
+    <React.Fragment>
+        <Navbar />
+    </React.Fragment>
+);
