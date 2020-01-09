9327aec7928f1c8712f5fb7e66b2092e20c7c1e6
buhtla85 <tamara.naunovic@gmail.com>
2020-01-09 23:32:11 +0100

    setup for contacts page

--- src/client/pages/ContactUs.tsx
+++ src/client/pages/ContactUs.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import {Navbar} from "../components/Navbar";
+import {Footer} from "../components/Footer";
+import {SectionStaff} from "../components/SectionStaff";
 
 export const ContactUs = (): JSX.Element => (
     <React.Fragment>
-        Hello from contacts page
+        <Navbar />
+        <SectionStaff />
+        <Footer content="Copyright &copy; 2020, Tamara"/>
     </React.Fragment>
 );
