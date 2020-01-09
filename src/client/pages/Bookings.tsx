512dfa6beaf52106608d0cef122e6b4873482d6a
buhtla85 <tamara.naunovic@gmail.com>
2020-01-09 23:16:01 +0100

    setup for bookings page finished

--- src/client/pages/Bookings.tsx
+++ src/client/pages/Bookings.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import {Navbar} from "../components/Navbar";
+import {Footer} from "../components/Footer";
+import {SectionTitle} from "../components/SectionTitle";
+import Reservation from "../components/Reservation";
 
 export const Bookings = (): JSX.Element => (
     <React.Fragment>
-        Hello from bookings page
+        <Navbar />
+        <section className="bookings">
+            <div className="customContainer">
+                <SectionTitle title="book a stay" styling="section-title1"/>
+                <Reservation />
+            </div>
+        </section>
+        <Footer content="Copyright &copy; 2020, Tamara" />
     </React.Fragment>
 );
