ba3ef5829366947476d0faa5189c5b677840ef79
buhtla85 <tamara.naunovic@gmail.com>
2020-01-09 19:56:12 +0100

    added hero section

---
+++ src/client/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Hero = (): JSX.Element => {
+    return (
+        <div className="hero-image">
+            <div className="hero-text">
+                <h1>Welcome to the Furball Hotel</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi qui aperiam, adipisci nesciunt fugiat rem magnam voluptatum voluptatibus at pariatur!</p>
+            </div>
+        </div>
+    )
+};
