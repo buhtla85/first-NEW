fb2406a1ac22e3e3f33ceb4c2ffef00fbe6ad85e
buhtla85 <tamara.naunovic@gmail.com>
2020-01-14 21:10:55 +0100

    tried something, did not work

--- src/client/components/CardStaff.tsx
+++ src/client/components/CardStaff.tsx
@@ -11,11 +11,11 @@ export const CardStaff = (props: IPropStaffCard): JSX.Element => {
     return (
         <div className="card">
             <img src={props.source} className="card-img-top" alt={props.alternative}/>
-            <div className="card-body">
-                <h4 className="card-title">{props.name}</h4>
-                <h5>{props.job}</h5>
-                <p className="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam nam quibusdam vitae beatae! Voluptates, aliquam.</p>
-            </div>
-        </div>  
+                <div className="card-body">
+                    <h4 className="card-title">{props.name}</h4>
+                    <h5>{props.job}</h5>
+                    <p className="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam nam quibusdam vitae beatae! Voluptates, aliquam.</p>
+                </div>
+        </div> 
     )
 };
