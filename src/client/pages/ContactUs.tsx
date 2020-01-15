122f99fc3cca0a192af0b7fd84abb293e5eacfc6
buhtla85 <tamara.naunovic@gmail.com>
2020-01-15 21:06:34 +0100

    added social media component

--- src/client/pages/ContactUs.tsx
+++ src/client/pages/ContactUs.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import {Navbar} from "../components/Navbar";
 import {Footer} from "../components/Footer";
 import {SectionStaff} from "../components/SectionStaff";
+import {SectionContacts} from "../components/SectionContacts";
 
 export const ContactUs = (): JSX.Element => (
     <React.Fragment>
         <Navbar />
         <SectionStaff />
+        <SectionContacts />
         <Footer content="Copyright &copy; 2020, Tamara"/>
     </React.Fragment>
 );
